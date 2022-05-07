b5e1c2e5020a3c5c4966716279da445255903dfa
Ahmed Abdelsalam <engahmedabdelsalam918@gmail.com>
2022-05-07 15:56:02 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+ 'use strict';
+
+ module.exports = {
+   useTabs: false,
+   printWidth: 100,
+   tabWidth: 2,
+   trailingComma: 'es5',
+   bracketSpacing: true,
+   bracketSameLine: false,
+   arrowParens: 'avoid',
+   vueIndentScriptAndStyle: true,
+   semi: false,
+ };
